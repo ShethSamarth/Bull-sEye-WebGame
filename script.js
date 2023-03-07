d7e7adb3b2682448c4d4626ca5e1e7d07947a111
ShethSamarth <samarth.sheth.29@gmail.com>
2023-03-07 21:49:03 +0530

    lesson11

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ window.addEventListener("load", function () {
       this.speedY = 0
       this.dx = 0
       this.dy = 0
-      this.speedModifier = 20
+      this.speedModifier = 5
     }
     draw(context) {
       context.beginPath()
@@ -54,8 +54,14 @@ window.addEventListener("load", function () {
       this.collisionY += this.speedY * this.speedModifier
       // collisions with obstacles
       this.game.obstacles.forEach((obstacle) => {
-        if (this.game.checkCollision(this, obstacle)) {
-          console.log("collision")
+        // [distance < sumOfRadii, distance, sumOfRadii, dx, dy]
+        let [collision, distance, sumOfRadii, dx, dy] =
+          this.game.checkCollision(this, obstacle)
+        if (collision) {
+          const unit_x = dx / distance
+          const unit_y = dy / distance
+          this.collisionX = obstacle.collisionX + (sumOfRadii + 1) * unit_x
+          this.collisionY = obstacle.collisionY + (sumOfRadii + 1) * unit_y
         }
       })
     }
@@ -139,16 +145,16 @@ window.addEventListener("load", function () {
       })
     }
     render(context) {
+      this.obstacles.forEach((obstacle) => obstacle.draw(context))
       this.player.draw(context)
       this.player.update()
-      this.obstacles.forEach((obstacle) => obstacle.draw(context))
     }
     checkCollision(a, b) {
       const dx = a.collisionX - b.collisionX
       const dy = a.collisionY - b.collisionY
       const distance = Math.hypot(dy, dx)
       const sumOfRadii = a.collisionRadius + b.collisionRadius
-      return distance < sumOfRadii
+      return [distance < sumOfRadii, distance, sumOfRadii, dx, dy]
     }
     init() {
       let attempts = 0
@@ -185,7 +191,7 @@ window.addEventListener("load", function () {
 
   const game = new Game(canvas)
   game.init()
-  console.log(game)
+  // console.log(game)
 
   function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
