8d80f75655c4054b5d37ab489116a5c3abe04eec
ShethSamarth <samarth.sheth.29@gmail.com>
2023-03-11 20:22:26 +0530

    lesson27

--- script.js
+++ script.js
@@ -178,7 +178,7 @@ window.addEventListener("load", function () {
       this.spriteX
       this.spriteY
       this.hatchTimer = 0
-      this.hatchInterval = 3000
+      this.hatchInterval = 10000
       this.markedForDeletion = false
     }
     draw(context) {
@@ -225,7 +225,10 @@ window.addEventListener("load", function () {
         }
       })
       // hatching
-      if (this.hatchTimer > this.hatchInterval) {
+      if (
+        this.hatchTimer > this.hatchInterval ||
+        this.collisionY < this.game.topMargin
+      ) {
         this.game.hatchlings.push(
           new Larva(this.game, this.collisionX, this.collisionY)
         )
@@ -315,6 +318,11 @@ window.addEventListener("load", function () {
           this.markedForDeletion = true
           this.game.removeGameObjects()
           this.game.lostHatchlings++
+          for (let i = 0; i < 5; i++) {
+            this.game.particles.push(
+              new Spark(this.game, this.collisionX, this.collisionY, "yellow")
+            )
+          }
         }
       })
     }
@@ -408,7 +416,7 @@ window.addEventListener("load", function () {
   class Firefly extends Particle {
     update() {
       this.angle += this.va
-      this.collisionX += this.speedX
+      this.collisionX += Math.cos(this.angle) * this.speedX
       this.collisionY -= this.speedY
       if (this.collisionY < 0 - this.radius) {
         this.markedForDeletion = true
@@ -417,7 +425,18 @@ window.addEventListener("load", function () {
     }
   }
 
-  class Spark extends Particle {}
+  class Spark extends Particle {
+    update() {
+      this.angle += this.va * 0.5
+      this.collisionX -= Math.sin(this.angle) * this.speedX
+      this.collisionY -= Math.cos(this.angle) * this.speedY
+      if (this.radius > 0.1) this.radius -= 0.05
+      if (this.radius < 0.2) {
+        this.markedForDeletion = true
+        this.game.removeGameObjects()
+      }
+    }
+  }
 
   class Game {
     constructor(canvas) {
