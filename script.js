482dcac677fff7df9e0c512b64010681ef8dce11
ShethSamarth <samarth.sheth.29@gmail.com>
2023-03-11 18:16:32 +0530

    lesson24

--- script.js
+++ script.js
@@ -251,12 +251,14 @@ window.addEventListener("load", function () {
       this.spriteX
       this.spriteY
       this.speedY = 1 + Math.random()
+      this.frameX = 0
+      this.frameY = Math.floor(Math.random() * 2)
     }
     draw(context) {
       context.drawImage(
         this.image,
-        0,
-        0,
+        this.frameX * this.spriteWidth,
+        this.frameY * this.spriteHeight,
         this.spriteWidth,
         this.spriteHeight,
         this.spriteX,
@@ -288,7 +290,28 @@ window.addEventListener("load", function () {
       if (this.collisionY < this.game.topMargin) {
         this.markedForDeletion = true
         this.game.removeGameObjects()
+        this.game.score++
       }
+      // collision with gameObjects
+      let collisionObjects = [this.game.player, ...this.game.obstacles]
+      collisionObjects.forEach((object) => {
+        let [collision, distance, sumOfRadii, dx, dy] =
+          this.game.checkCollision(this, object)
+        if (collision) {
+          const unit_x = dx / distance
+          const unit_y = dy / distance
+          this.collisionX = object.collisionX + [sumOfRadii + 1] * unit_x
+          this.collisionY = object.collisionY + [sumOfRadii + 1] * unit_y
+        }
+      })
+      // collision with enemies
+      this.game.enemies.forEach((enemy) => {
+        if (this.game.checkCollision(this, enemy)[0]) {
+          this.markedForDeletion = true
+          this.game.removeGameObjects()
+          this.game.lostHatchlings++
+        }
+      })
     }
   }
 
@@ -373,6 +396,8 @@ window.addEventListener("load", function () {
       this.enemies = []
       this.hatchlings = []
       this.gameObjects = []
+      this.score = 0
+      this.lostHatchlings = 0
       this.mouse = {
         x: this.width * 0.5,
         y: this.height * 0.5,
